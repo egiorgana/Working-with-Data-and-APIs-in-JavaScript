f0e6be251f0d1307e5ebf3e6ecf7ad11391fcc9a
egiorgana <eagiorganamorales@gmail.com>
2020-06-14 19:57:28 -0500

    Updated API data per second in the browser

--- client-side-basics/fetch_JS0N/main.js
+++ client-side-basics/fetch_JS0N/main.js
@@ -1,4 +1,8 @@
-const mymap = L.map('mapid').setView([0, 0], 1);
+// MAKING A MAP AND TILES
+const mymap = L.map('mapid').setView([0, 0], 0);
+const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreenMap</a> contributors'
+
+// MAKING A MARKER WITH A CUSTOM ICON
 const issIcon = L.icon({
     iconUrl: 'ISS.svg.png',
     iconSize: [50, 32],
@@ -8,19 +12,27 @@ const issIcon = L.icon({
 
 const marker = L.marker([0, 0], {icon: issIcon}).addTo(mymap)
 
-const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreenMap</a> contributors'
 const tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const tiles = L.tileLayer(tileUrl, { attribution })
 tiles.addTo(mymap)
 
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
 
-getISS()
+let firstTime = true
+
 async function getISS(){
     const response = await fetch(api_url)
     const data = await response.json()
     const { latitude, longitude } = data
     marker.setLatLng([latitude, longitude])
-    document.getElementById('lat').textContent = latitude
-    document.getElementById('long').textContent = longitude
+    if(firstTime){
+        mymap.setView([latitude, longitude], 2)
+        firstTime = false
+    }
+    document.getElementById('lat').textContent = latitude.toFixed(2)
+    document.getElementById('long').textContent = longitude.toFixed(2)
 }
+
+getISS()
+
+setInterval(getISS, 1000)
